b11939922577e5fa7814ffc47c17b0b3b40716d5
Kaloyan Georgiev <kaloyan.georgiev01@sap.com>
2023-12-27 20:44:16 +0200

    refactor: improve error handling

--- src/plugins/errorHandler.plugin.ts
+++ src/plugins/errorHandler.plugin.ts
@@ -3,19 +3,19 @@ import logger from "../utils/logger";
 import { CollectionError } from "../errors/errors";
 
 export const errorHandlerPlugin
-    = new Elysia().onError(({ error, set }) => {
+    = new Elysia().onError(({ code, error, set }) => {
 
-        switch(true){
+        switch (true) {
             case error instanceof CollectionError:
                 set.status = 404;
                 break;
+            case code === "VALIDATION":
+                set.status = 400;
+                return { message: "Invalid data" }
             default:
                 logger.error(error);
                 set.status = 500;
         }
 
-        return {
-            message: error.message
-        };
+        return error.message;
     })
-
